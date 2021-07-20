bd948d1adb4769c38381c9cdbaad0f740606f768
Giacomo Rizzi <gufoes@gmail.com>
2021-07-20 17:31:04 +0200

    added relSync

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -8,67 +8,67 @@ config({ path: resolve(__dirname, "../.env") })
 
 let api: Api = null
 
-// describe("SETUP", async () => {
-//   it("Schema request", async () => {
-//     api = new Api(process.env.COMPANY, process.env.TOKEN);
-//     await api.loadSchema()
-//     expect(api.getRequestCount()).to.equal(1);
-//   });
-// });
+describe("SETUP", async () => {
+  it("Schema request", async () => {
+    api = new Api(process.env.COMPANY, process.env.TOKEN);
+    await api.loadSchema()
+    expect(api.getRequestCount()).to.equal(1);
+  });
+});
 
-// describe("GET ONE", () => {
-//   it("First capitolo request", async () => {
-//     let res = await api.query('capitoli').first()
-//     let capitolo = res.json;
-//     expect(capitolo.id).to.equal(236826)
-//   });
-// })
+describe("GET ONE", () => {
+  it("First capitolo request", async () => {
+    let res = await api.query('capitoli').first()
+    let capitolo = res.json;
+    expect(capitolo.id).to.equal(236826)
+  });
+})
 
-// describe("FILE", () => {
-//   it("Verify file url", async () => {
-//     let arg = await api.query('argomenti').first();
-//     let img = arg.val('disegno1');
-//     expect(img).to.be.instanceOf(OpFile);
-//     expect(img.link()).to.equal('https://lithos.onpage.it/api/storage/PMWJiNp8eYn2Hy3TevNU?name=shutterstock_36442114-ok-NEW.jpg')
-//   });
-// });
+describe("FILE", () => {
+  it("Verify file url", async () => {
+    let arg = await api.query('argomenti').first();
+    let img = arg.val('disegno1');
+    expect(img).to.be.instanceOf(OpFile);
+    expect(img.link()).to.equal('https://lithos.onpage.it/api/storage/PMWJiNp8eYn2Hy3TevNU?name=shutterstock_36442114-ok-NEW.jpg')
+  });
+});
 
-// describe("GET ALL", () => {
-//   it("All capitoli request", async () => {
-//     let caps = await api.query('capitoli').all();
-//     expect(caps.length).to.equal(23);
-//   });
-// });
+describe("GET ALL", () => {
+  it("All capitoli request", async () => {
+    let caps = await api.query('capitoli').all();
+    expect(caps.length).to.equal(23);
+  });
+});
 
-// describe("FILTERS", () => {
-//   it("Get capitolo by id or value", async () => {
-//     let cap = await api.query('capitoli').where('_id', 236826).first();
-//     expect(cap.id).to.equal(236826);
-//     let cap2 = await api.query('capitoli').where('descrizione', 'like', 'led').first();
-//     expect(cap2.id).to.equal(236827);
-//   });
-// });
+describe("FILTERS", () => {
+  it("Get capitolo by id or value", async () => {
+    let cap = await api.query('capitoli').where('_id', 236826).first();
+    expect(cap.id).to.equal(236826);
+    let cap2 = await api.query('capitoli').where('descrizione', 'like', 'led').first();
+    expect(cap2.id).to.equal(236827);
+  });
+});
 
-// describe("RELATION", () => {
-//   it("Get one resource related and its values", async () => {
-//     let cap = await api.query('capitoli').first();
-//     api.resetRequestCount()
-//     checkArgomenti(cap);
-//     expect(api.getRequestCount()).to.equal(1);
-//   });
-// });
+describe("RELATION", () => {
+  it("Get one resource related and its values", async () => {
+    let cap = await api.query('capitoli').first();
+    api.resetRequestCount()
+    checkArgomenti(cap);
+    expect(api.getRequestCount()).to.equal(1);
+  });
+});
 
-// describe("NESTED RELATIONS", () => {
-//   it("Get resources related with many degrees of separation", async () => {
-//     let cap = await api.query('capitoli').first();
-//     api.resetRequestCount()
-//     let arts = await cap.rel('argomenti.prodotti.articoli')
-//     let prods = await cap.rel('argomenti.prodotti')
-//     expect(api.getRequestCount()).to.equal(1);
-//     expect(prods.length).to.equal(23);
-//     expect(arts.length).to.equal(76);
-//   });
-// });
+describe("NESTED RELATIONS", () => {
+  it("Get resources related with many degrees of separation", async () => {
+    let cap = await api.query('capitoli').first();
+    api.resetRequestCount()
+    let arts = await cap.rel('argomenti.prodotti.articoli')
+    let prods = await cap.rel('argomenti.prodotti')
+    expect(api.getRequestCount()).to.equal(1);
+    expect(prods.length).to.equal(23);
+    expect(arts.length).to.equal(76);
+  });
+});
 
 describe("NESTED RELATIONS ONDEMAND", () => {
   it("Verify preload relations", async () => {
@@ -83,7 +83,7 @@ describe("NESTED RELATIONS ONDEMAND", () => {
     expect(prods.length).to.equal(23);
     expect(arts.length).to.equal(76);
     let prods_sync = cap.relSync('argomenti.prodotti')
-    expect(prods_sync).to.equal(prods);
+    expect(prods_sync).to.have.members(prods);
 
   });
 });
