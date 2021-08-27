311554e39446ca95b27534d4277a9d97d3b2ea44
Giacomo Rizzi <giacomo.rizzi@onpage.it>
2021-08-27 11:42:03 +0200

    Correct fields indentation

--- src/fields.ts
+++ src/fields.ts
@@ -1,40 +1,40 @@
 const DIM2_FIELDS = [
-    {
-        type: 'real',
-        label: 'x',
-    },
-    {
-        type: 'real',
-        label: 'y'
-    }
+  {
+    type: 'real',
+    label: 'x',
+  },
+  {
+    type: 'real',
+    label: 'y'
+  }
 ]
 export type Dim2Field = (typeof DIM2_FIELDS)[number]
 
 const DIM3_FIELDS = [
-    {
-        type: 'real',
-        label: 'x',
-    },
-    {
-        type: 'real',
-        label: 'y'
-    },
-    {
-        type: 'real',
-        label: 'z'
-    }
+  {
+    type: 'real',
+    label: 'x',
+  },
+  {
+    type: 'real',
+    label: 'y'
+  },
+  {
+    type: 'real',
+    label: 'z'
+  }
 ]
 export type Dim3Field = (typeof DIM3_FIELDS)[number]
 
 const FILE_FIELDS = [
-    {
-        type: 'string',
-        label: 'name',
-    },
-    {
-        type: 'string',
-        label: 'token'
-    }
+  {
+    type: 'string',
+    label: 'name',
+  },
+  {
+    type: 'string',
+    label: 'token'
+  }
 ]
 export type FileField = (typeof FILE_FIELDS)[number]
 
@@ -45,24 +45,24 @@ const BOOL_FIELDS = null
 export type SubField = Dim2Field | Dim3Field | FileField | null
 
 export const SubFields = new Map([
-    ['string', STRING_FIELDS],
-    ['text', STRING_FIELDS],
-    ['paragraph', STRING_FIELDS],
-    ['markdown', STRING_FIELDS],
-    ['html', STRING_FIELDS],
-    ['url', STRING_FIELDS],
-    ['int', NUMBER_FIELDS],
-    ['real', NUMBER_FIELDS],
-    ['dim1', NUMBER_FIELDS],
-    ['dim2', DIM2_FIELDS],
-    ['dim3', DIM3_FIELDS],
-    ['volume', NUMBER_FIELDS],
-    ['weight', NUMBER_FIELDS],
-    ['price', NUMBER_FIELDS],
-    ['bool', BOOL_FIELDS],
-    ['date', STRING_FIELDS],
-    ['json', STRING_FIELDS],
-    ['editorjs', STRING_FIELDS],
-    ['file', FILE_FIELDS],
-    ['image', FILE_FIELDS]
+  ['string', STRING_FIELDS],
+  ['text', STRING_FIELDS],
+  ['paragraph', STRING_FIELDS],
+  ['markdown', STRING_FIELDS],
+  ['html', STRING_FIELDS],
+  ['url', STRING_FIELDS],
+  ['int', NUMBER_FIELDS],
+  ['real', NUMBER_FIELDS],
+  ['dim1', NUMBER_FIELDS],
+  ['dim2', DIM2_FIELDS],
+  ['dim3', DIM3_FIELDS],
+  ['volume', NUMBER_FIELDS],
+  ['weight', NUMBER_FIELDS],
+  ['price', NUMBER_FIELDS],
+  ['bool', BOOL_FIELDS],
+  ['date', STRING_FIELDS],
+  ['json', STRING_FIELDS],
+  ['editorjs', STRING_FIELDS],
+  ['file', FILE_FIELDS],
+  ['image', FILE_FIELDS]
 ])
